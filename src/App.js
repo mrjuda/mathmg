1b0564155895676725b6f9cf1243a4d700261e00
judah <87juda@gmail.com>
2022-04-23 14:38:11 -0300

    Fix typos

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome(usr) {
+  return <p>Hello, {usr.name} ({usr.position}), welcome to JudaLabs!<br></br></p>;
+}
+
+function HiThere() {
+  return (
+      <Welcome name="Judá" position="CEO" />
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <HiThere />
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
