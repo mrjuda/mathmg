284de6cfa666902293d5bbeb9eff8afe330047b0
judah <87juda@gmail.com>
2022-04-23 16:12:48 -0300

    Add hooks' linters settings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@
 
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+function Example() {
+  // Declare a new state variable, which we'll call "count"  
+  const [count, setCount] = useState(0);
+  // Similar to componentDidMount and componentDidUpdate:  
+  useEffect(() => {    
+    // Update the document title using the browser API    
+    document.title = `JL ${count} times`;  
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
 
 function Welcome(usr) {
   return <p>Hello, {usr.name} ({usr.position}), welcome to JudaLabs!<br></br></p>;
@@ -19,6 +39,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <HiThere />
+        <Example />
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
