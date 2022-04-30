d179b1970a3f6a5b35930ce33930c567cca94ed4
judah <87juda@gmail.com>
2022-04-30 04:36:04 -0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-// import Calc from './components/Calc';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
